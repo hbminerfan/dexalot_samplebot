87b5c91f7770e03f344450cd29c599934965fa2e
hbminerfan <134588875+hbminerfan@users.noreply.github.com>
2023-11-22 11:34:55 +0800

    Update botLauncher.ts
    added single quotes in console log

--- services/bots/botLauncher.ts
+++ services/bots/botLauncher.ts
@@ -34,34 +34,25 @@ client.send(
     bot.start();
   });
   // exit handler to remove all open buy and sell orders from all simulators
-  process.on("SIGINT", async () => {
-    // Needed for Local Windows Ctrl-C
-    cleanUpAndExit(bot);
-  });
+process.on("SIGINT", async () => {
+  // Needed for Local Windows Ctrl-C
+  cleanUpAndExit(bot);
+});
 
-  process.on("exit", async () => {
-    cleanUpAndExit(bot);
-  });
+process.on("exit", async () => {
+  cleanUpAndExit(bot);
+});
 
-  process.on("SIGTERM", async () => {
-    //Needed for AWS
-    cleanUpAndExit(bot);
-  });
+process.on("SIGTERM", async () => {
+  //Needed for AWS
+  cleanUpAndExit(bot);
+});
 
-  process.on("uncaughtException", (error) => {
-    console.log(BotManager uncaughtException happened:, error);
-    bot.cancelOrderList([], 100);
-  });
+process.on("uncaughtException", (error) => {
+  console.log(`BotManager uncaughtException happened:`, error);
+});
 
-  process.on("unhandledRejection", (error, promise) => {
-    console.log(BotManager We forgot to handle a promise rejection here:, promise);
-    console.log(BotManager The error was:, error);
-    bot.cancelOrderList([], 100);
-  });
-
-
-  }).catch((error) => {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
-})
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`BotManager We forgot to handle a promise rejection here:`, promise);
+  console.log(`BotManager The error was:`, error);
+});
